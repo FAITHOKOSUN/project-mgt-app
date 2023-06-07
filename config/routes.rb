e58c895d282773f74337b2404380a4dd27a357e4
FAITHOKOSUN <okosunfaith2@gmail.com>
2023-06-07 03:38:50 +0100

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  # get 'about' => "pages#about"
   # Defines the root path route ("/")
   # root "articles#index"
 end
