9078b2842c77241dcfc5afcf31412c420bf02116
FAITHOKOSUN <okosunfaith2@gmail.com>
2023-06-07 16:03:49 +0100

    updated

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,8 @@ module ProjectMg
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+   
+   # config.eager_load_paths << Rails.root.join("extras")
+   config.app_generators.scaffold_controller = :scaffold_controller
   end
 end
