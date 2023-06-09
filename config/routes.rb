7d796957e4d0f49ed65210ae201e19f483d00279
FAITHOKOSUN <okosunfaith2@gmail.com>
2023-06-09 07:12:34 +0100

    updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
   resources :tasks
   ActiveAdmin.routes(self)
-  devise_for :users
-  root 'tasks#index'
+  
+     devise_for :users
+     root 'pages#home'
+    
+
 
   devise_scope :user do
     get 'users/sign_out' => "devise/sessions#destroy"
 end
-patch 'tasks/update_name', to: 'tasks#update_name', as: :update_name
 
+patch 'tasks/update_name', to: 'tasks#update_name', as: :update_name
+  get 'tasks', to: 'tasks#index', as: :tasks_index
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
